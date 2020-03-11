b75b5589f93d10e2f6d72282099bc0c7b92c6cea
Dilitand <Dilitand@gmail.com>
2020-03-11 21:43:40 +0300

    4 lesson

---
+++ src/main/java/Event.java
@@ -0,0 +1,29 @@
+import javax.swing.text.DateFormatter;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class Event {
+    int id = new Random().nextInt() * 100;
+    String msg;
+    Date date;
+    DateFormat dateFormat;
+
+    public Event(Date date, DateFormat dateFormat) {
+        this.date = date;
+        this.dateFormat = dateFormat;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "id = " + id + " msg = " + msg + " date = " + dateFormat.format(date);
+    }
+}
